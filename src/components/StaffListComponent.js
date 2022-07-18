c1d5293877ba81359473ce90cb1cefbc008fe985
Minh Hoang <daominhhoangggg@gmail.com>
2022-07-18 16:01:28 +0700

    add images

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,14 +1,6 @@
 import React, { Component } from "react";
-import {
-  Card,
-  CardImg,
-  CardImgOverlay,
-  CardText,
-  CardBody,
-  CardTitle,
-} from "reactstrap";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 import dateFormat from "dateformat";
-import { DEPARTMENTS } from "../shared/staffs";
 
 class Menu extends Component {
   constructor(props) {
@@ -23,11 +15,28 @@ class Menu extends Component {
     this.setState({ selectedStaff: staff });
   }
 
+  renderImage(staff) {
+    if (staff != null) {
+      return (
+        <div className="col-12 col-md-2">
+          <CardImg
+            className="mt-2"
+            width="120%"
+            src={staff.image}
+            alt={staff.name}
+          />
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+
   renderStaff(staff) {
     if (staff != null) {
       return (
-        <div className="col-12 col-md-5 m-1">
-          <Card>
+        <div className="col-12 col-md-4 my-2">
+          <div>
             <CardBody>
               <CardTitle>Họ và tên: {staff.name}</CardTitle>
               <CardText>
@@ -40,7 +49,7 @@ class Menu extends Component {
               <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
               <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
             </CardBody>
-          </Card>
+          </div>
         </div>
       );
     } else {
@@ -55,9 +64,9 @@ class Menu extends Component {
   render() {
     const menu = this.props.staffs.map((staff) => {
       return (
-        <div key={staff.id} className="col-12 col-md-4 my-2">
+        <div key={staff.id} className="col-12 col-md-4 my-2 pl-2">
           <Card onClick={() => this.staffSelect(staff)}>
-            <CardTitle>{staff.name}</CardTitle>
+            <CardTitle className="pl-2 pt-2">{staff.name}</CardTitle>
           </Card>
         </div>
       );
@@ -67,6 +76,7 @@ class Menu extends Component {
       <div className="container">
         <div className="row">{menu}</div>
         <div className="row" onClick={() => this.staffSelect(null)}>
+          {this.renderImage(this.state.selectedStaff)}
           {this.renderStaff(this.state.selectedStaff)}
         </div>
       </div>
